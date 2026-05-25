592f33650532bbdd3494a89dca6b4195276a7528
srish <srish@HP>
2026-05-25 23:24:48 +0530

    Added working Calculator.java file

---
+++ src/main/java/NewClass.java
@@ -0,0 +1,60 @@
+
+import java.util.*;
+        
+public class NewClass {
+    
+    public static void main(String args[]){
+        System.out.print("hello");
+        System.out.println("hello");
+        System.out.print("hello\nriya\n");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        Scanner Sc = new Scanner(System.in);//input
+        String name = Sc.nextLine();//next int nextfloat
+        System.out.println(name);
+        Scanner Scs = new Scanner(System.in);
+        int a = Scs.nextInt();
+        int b = Scs.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+        
+        
+        
+        
+        
+        
+        
+/*
+        how code run in jdk
+        -compilation-jre(java run time environment)-->jvm(java virtual machine)
+        cade we wrie is called source code tthen code is send to compiler ....compiler convert source cd into byte code
+        byte code can run in any os which has jre(so java is portable lang)
+        
+        -execution-
+        jvm convert byte code into native code
+        
+        java is typed lang - where data type should be predefined
+        --premitive data type-8
+        byte-1byte-8bits-
+        char-2
+        boolean-1
+        int-4
+        float-4
+        long-8
+        double-8
+        sort-
+        --non premitive-string array class object interface
+        
+        priority--mul div modula>plus minus
+        
+        
+        
+        
+        
+        
+        
+        */
+    }
+}
